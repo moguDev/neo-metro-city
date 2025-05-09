de224fe7d0542b151c60f8e3c96f8ff55590bb8d
moguDev <mail@sho-horiguchi.jp>
2025-05-10 00:40:46 +0900

    fix: footer and library-pathes for hero badge

--- packages/docs/src/components/Footer.tsx
+++ packages/docs/src/components/Footer.tsx
@@ -3,47 +3,11 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full overflow-hidden h-56 mt-32 p-6 select-none bg-cyber-dark">
-      <div className="flex md:flex-row flex-col items-center justify-between gap-4 h-full">
-        <div className="my-auto">
-          <p className="text-sm font-bold">Neo MeTRO Labs.</p>
-          <div className="flex items-center gap-6 my-4">
-            <div className="flex flex-col items-center gap-3 w-20">
-              <Image
-                src="https://avatars.githubusercontent.com/u/12319280?v=4"
-                alt="mogu"
-                className="rounded-[20px]"
-                width={60}
-                height={60}
-              />
-              <p className="text-sm font-bold">MOGU</p>
-            </div>
-            <div className="flex flex-col items-center gap-3 w-20">
-              <Image
-                src="https://avatars.githubusercontent.com/u/168655536?v=4"
-                alt="potatoman"
-                className="rounded-[20px]"
-                width={60}
-                height={60}
-              />
-              <p className="text-sm font-bold">POTATOMAN</p>
-            </div>
-            <div className="flex flex-col items-center gap-3 w-20">
-              <Image
-                src="https://avatars.githubusercontent.com/u/168791433?v=4"
-                alt="matsu"
-                className="rounded-[20px]"
-                width={60}
-                height={60}
-              />
-              <p className="text-sm font-bold">MATSU</p>
-            </div>
-          </div>
-        </div>
-        <p className="tracking-wider md:text-sm text-xs">
-          Maintananced by Neo MeTRO Labs and Contributors.
-        </p>
-      </div>
+    <footer className="w-full overflow-hidden h-fit mt-32 p-3 select-none">
+      <p className="md:text-sm text-xs text-center italic font-medium neon-glow-soft tracking-widest">
+        Maintananced by <span className="text-neon-orange">Neo Metro Labs</span>{" "}
+        and <span className="text-neon-pink">Contributors</span>.
+      </p>
     </footer>
   );
 };
