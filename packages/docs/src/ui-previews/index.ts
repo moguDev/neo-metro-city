ddde826b5efaf176166074df9317736545adda5f
moguDev <mail@sho-horiguchi.jp>
2025-05-09 11:46:51 +0900

    add some components previews

--- packages/docs/src/ui-previews/index.ts
+++ packages/docs/src/ui-previews/index.ts
@@ -13,20 +13,32 @@ import swapPreviewData from "./swapPreviewData";
 import tabPreviewData from "./tabPreviewData";
 import togglePreviewData from "./togglePreviewData";
 import progressPreviewData from "./progressPreviewData";
+import textareaPreviewData from "./textareaPreviewData";
+import loadingPreviewData from "./loadingPreviewData";
+import badgePreviewData from "./badgePreviewData";
+import statusPreviewData from "./statusPreviewData";
+import filedsetPreviewData from "./fieldsetPreviewData";
+import selectPreviewData from "./selectPreviewData";
 
 export const componentsPreviewDatas: ComponentPreviewData[] = [
   accordionPreviewData,
   alertPreviewData,
+  badgePreviewData,
   buttonPreviewData,
   checkboxPreviewData,
   drawerPreviewData,
   dropdownPreviewData,
+  filedsetPreviewData,
   inputfieldPreviewData,
   listPreviewData,
+  loadingPreviewData,
   modalPreviewData,
   progressPreviewData,
   radioPreviewData,
+  selectPreviewData,
+  statusPreviewData,
   swapPreviewData,
   tabPreviewData,
+  textareaPreviewData,
   togglePreviewData,
 ];
