776b4725259259e17be23304b525c21e25d987b1
moguDev <mail@sho-horiguchi.jp>
2025-05-11 19:08:11 +0900

    upate footer

--- packages/docs/src/components/Footer.tsx
+++ packages/docs/src/components/Footer.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <footer className="w-full overflow-hidden h-fit mt-32 p-3 select-none">
-      <p className="md:text-sm text-xs text-center italic font-medium neon-glow-soft tracking-widest">
-        Maintananced by <span className="text-neon-orange">Neo Metro Labs</span>{" "}
-        and <span className="text-neon-pink">Contributors</span>.
-      </p>
+    <footer className="w-full overflow-hidden h-fit mt-32 p-5 select-none border-t border-gray-900 bg-cyber-black">
+      <div className="max-w-7xl mx-auto flex flex-col md:items-start items-center">
+        <Image
+          src="/logo_neometrolabs.webp"
+          alt="logo"
+          width={160}
+          height={80}
+        />
+        <p className="text-xs p-1 italic font-medium neon-glow-soft tracking-widest">
+          Maintananced by Neo Metro Labs and Contributors.
+        </p>
+      </div>
     </footer>
   );
 };
