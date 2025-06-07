10b18499c9476afe1876c0006a6546dcdc17c98e
topi0247 <karubona.ra247@gmail.com>
2025-06-07 21:59:25 +0900

    feat: add badge color variants

--- packages/docs/src/ui-previews/badgePreviewData.tsx
+++ packages/docs/src/ui-previews/badgePreviewData.tsx
@@ -12,6 +12,37 @@ const badgePreviewData: ComponentPreviewData = {
       htmlStr: dedent(`<span className="badge">BADGE</span>`),
       jsxStr: dedent(`<span className="badge">BADGE</span>`),
     },
+    {
+      title: "Badge colors",
+      component: (
+        <div className="flex flex-wrap gap-4">
+          <span className="badge">NEUTRAL</span>
+          <span className="badge badge-orange">ORANGE</span>
+          <span className="badge badge-pink">PINK</span>
+          <span className="badge badge-green">GREEN</span>
+          <span className="badge badge-red">RED</span>
+          <span className="badge badge-yellow">YELLOW</span>
+        </div>
+      ),
+      htmlStr: dedent(`
+        <div className="flex flex-wrap gap-4">
+          <span className="badge">NEUTRAL</span>
+          <span className="badge badge-orange">ORANGE</span>
+          <span className="badge badge-pink">PINK</span>
+          <span className="badge badge-green">GREEN</span>
+          <span className="badge badge-red">RED</span>
+          <span className="badge badge-yellow">YELLOW</span>
+        </div>`),
+      jsxStr: dedent(`
+        <div className="flex flex-wrap gap-4">
+          <span className="badge">NEUTRAL</span>
+          <span className="badge badge-orange">ORANGE</span>
+          <span className="badge badge-pink">PINK</span>
+          <span className="badge badge-green">GREEN</span>
+          <span className="badge badge-red">RED</span>
+          <span className="badge badge-yellow">YELLOW</span>
+        </div>`),
+    },
   ],
 };
 
